b879fa514db33bc25bb071569a0eef0316e8fe8f
wendelca <wendelca@gmail.com>
2022-10-28 15:13:48 -0300

    Primeiro commit

---
+++ Documents/Ides/SpringEmailDemo/SpringEmailDemo/src/main/java/com/emailservice/SpringEmailDemo/SpringEmailDemoApplication.java
@@ -0,0 +1,27 @@
+package com.emailservice.SpringEmailDemo;
+
+import javax.mail.MessagingException;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.event.ApplicationReadyEvent;
+import org.springframework.context.event.EventListener;
+
+@SpringBootApplication
+public class SpringEmailDemoApplication {
+
+	@Autowired
+	private EmailSenderService senderService;
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringEmailDemoApplication.class, args);
+
+	}
+
+	@EventListener(ApplicationReadyEvent.class)
+	public void triggerMail() throws MessagingException {
+		senderService.sendSimpleEmail("wendelca@gmail.com", "Este é email teste spring email sender", "Teste de email");
+
+	}
+}
